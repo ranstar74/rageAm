58649e34d195ce73c8cc08c4987eac9f33657014
ranstar74 <falconr1125@gmail.com>
2023-09-29 18:03:58 +0300

    Add base fields of at pool

--- projects/app/src/rage/atl/pool.h
+++ projects/app/src/rage/atl/pool.h
@@ -7,7 +7,25 @@
 //
 #pragma once
 
+#include "common/types.h"
+
 namespace rage
 {
-	
+	class atPoolBase
+	{
+		struct FreeNode
+		{
+			FreeNode* Next;
+		};
+
+		char* m_Storage;
+		u64 m_TotalCount;
+		u64 m_FreeCount;
+		u64 m_ElementSize;
+		FreeNode* m_FreeHead;
+		bool m_OwnMemory;
+		u32* m_Infos;
+		u16 m_InfoSize;
+		u16 m_Size;
+	};
 }
