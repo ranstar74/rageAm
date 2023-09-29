98bba1ac0e17b1e9310b4164a76679e397889d1f
ranstar74 <falconr1125@gmail.com>
2023-09-29 18:00:21 +0300

    Fix ref counter not destructing object

--- projects/app/src/rage/paging/place.h
+++ projects/app/src/rage/paging/place.h
@@ -10,21 +10,36 @@
 #define IMPLEMENT_REF_COUNTER(name_) \
 	void AddRef() { \
 		_InterlockedIncrement(&m_RefCount); \
-		AM_DEBUGF("AddRef(%s, %#llx)", typeid(this).name(), (u64)this); \
+		/*AM_DEBUGF("AddRef(%s, %#llx)", typeid(this).name(), (u64)this);*/ \
 	} \
-	u32 Release() { \
-		AM_DEBUGF("Release(%s, %#llx) -> RefCount: %u", typeid(this).name(), (u64)(this), m_RefCount - 1); \
-		u32 refs = _InterlockedDecrement(&m_RefCount); \
-		if(refs == 0) this->~name_(); \
+	auto Release() { \
+		/*AM_DEBUGF("Release(%s, %#llx) -> RefCount: %u", typeid(this).name(), (u64)(this), m_RefCount - 1);*/ \
+		auto refs = _InterlockedDecrement(&m_RefCount); \
+		if(refs == 0) delete this;/* this->~name_(); */ \
 		return refs; \
 	} \
-	u32 GetNumRefs() const { return m_RefCount; } \
+	auto GetNumRefs() const { return m_RefCount; } \
 	MACRO_END
 
-/**
- * \brief Implements place / allocate functions and new / delete operator overloads for resource building.
- * \param name Name of resource class.
- */
+ /**
+  * \brief 16 bit (short) ref counter, used in crSkeleton and crJointData, maybe somewhere else...
+  */
+#define IMPLEMENT_REF_COUNTER16(name_) \
+	void AddRef() { \
+		_InterlockedIncrement16(&m_RefCount); \
+	} \
+	auto Release() { \
+		auto refs = _InterlockedDecrement16(&m_RefCount); \
+		if(refs == 0) delete this;/* this->~name_(); */ \
+		return refs; \
+	} \
+	auto GetNumRefs() const { return m_RefCount; } \
+	MACRO_END
+
+  /**
+   * \brief Implements place / allocate functions and new / delete operator overloads for resource building.
+   * \param name Name of resource class.
+   */
 #define IMPLEMENT_PLACE_INLINE(name) \
 		void* operator new(std::size_t size) \
 		{ \
@@ -42,7 +57,7 @@
 		} \
 		static name* Place(const rage::datResource& rsc, name* that) \
 		{ \
-			rsc.Place(that); \
+			if(that) rsc.Place(that); \
 			return that; \
 		} \
 		static name* Allocate() \
