b947ea3513f2ebf87559f253b0207e99b3e48089
ranstar74 <falconr1125@gmail.com>
2023-10-04 21:18:49 +0300

    Add more im3d functionality & gizmo behaviour

--- projects/app/src/am/ui/im3d.cpp
+++ projects/app/src/am/ui/im3d.cpp
@@ -2,6 +2,7 @@
 
 #include "ImGuizmo.h"
 #include "imgui_internal.h"
+#include "am/graphics/shapetest.h"
 #include "game/viewport.h"
 #include "rage/grcore/device.h"
 
@@ -9,41 +10,59 @@
 
 constexpr ImU32 bgColor = IM_COL32(0, 0, 0, 140);
 
+static inline bool s_CenterNext = false;
+
 static auto GetDrawList()
 {
 	return ImGui::GetForegroundDrawList();
 }
 
-ImVec2 Im3D::WorldToScreen(const ImVec3V& pos, bool& isCulled)
+bool Im3D::WorldToScreen(const ImVec3V& worldPos, ImVec2& screenPos)
 {
 	u32 width, height;
 	rage::grcDevice::GetScreenSize(width, height);
 
-	rage::Vec4V vec = pos.Transform4(CViewport::GetViewProjectionMatrix());
+	rage::Vec4V vec = worldPos.Transform4(CViewport::GetViewProjectionMatrix());
 
 	// Check if position is behind of the screen
-	isCulled = vec.Z() < 0.0f;
+	bool isCulled = vec.Z() < 0.0f;
 
+	// TODO: Replace on remap, add helper in CViewport?
 	vec *= rage::S_HALF / vec.W();
 	vec += rage::Vec4V(0.5f, 0.5f, 1.0f, 1.0f);
 	vec.SetY(1.0f - vec.Y());
 	vec *= rage::Vec4V(static_cast<float>(width), static_cast<float>(height), 1.0f, 1.0f);
 
-	return ImVec2(vec.X(), vec.Y());
+	screenPos = ImVec2(vec.X(), vec.Y());
+	return !isCulled;
+}
+
+void Im3D::CenterNext()
+{
+	s_CenterNext = true;
 }
 
 void Im3D::Text(const ImVec3V& pos, ConstString fmt, ...)
 {
+	bool centerNext = s_CenterNext;
+	s_CenterNext = false;
+
 	va_list args;
 	va_start(args, fmt);
 	const char* text, * text_end;
 	ImFormatStringToTempBufferV(&text, &text_end, fmt, args);
 	va_end(args);
 
-	bool isCulled;
-	ImVec2 sPos = WorldToScreen(pos, isCulled);
-	if (isCulled)
+	ImVec2 sPos;
+	if (!WorldToScreen(pos, sPos))
 		return;
+
+	if (centerNext)
+	{
+		ImVec2 textSize = ImGui::CalcTextSize(text, text_end);
+		sPos -= { textSize.x * 0.5f, textSize.y * 0.5f };
+	}
+
 	GetDrawList()->AddText(sPos, ImGui::GetColorU32(ImGuiCol_Text), text, text_end);
 }
 
@@ -57,16 +76,23 @@ void Im3D::TextBg(const ImVec3V& pos, ConstString fmt, ...)
 
 void Im3D::TextBgV(const ImVec3V& pos, ConstString fmt, va_list args)
 {
+	bool centerNext = s_CenterNext;
+	s_CenterNext = false;
+
 	const char* text, * text_end;
 	ImFormatStringToTempBufferV(&text, &text_end, fmt, args);
 
-	bool isCulled;
-	ImVec2 sPos = WorldToScreen(pos, isCulled);
-	if (isCulled)
+	ImVec2 sPos;
+	if (!WorldToScreen(pos, sPos))
 		return;
 
-	// Black rectangle background for text
 	ImVec2 textSize = ImGui::CalcTextSize(text, text_end);
+	if (centerNext)
+	{
+		sPos -= { textSize.x * 0.5f, textSize.y * 0.5f };
+	}
+
+	// Black rectangle background for text
 	ImVec2 bgFrom = sPos;
 	ImVec2 bgTo = sPos + textSize;
 	GetDrawList()->AddRectFilled(bgFrom, bgTo, bgColor);
@@ -93,6 +119,17 @@ bool Im3D::GizmoTrans(ImMat44V& mtx)
 		GUIZMO_MTX(mtx));
 }
 
+bool Im3D::Gizmo(const ImMat44V& mtx, ImMat44V& delta, ImGuizmo::OPERATION op)
+{
+	return Manipulate(
+		GUIZMO_MTX(CViewport::GetViewMatrix()),
+		GUIZMO_MTX(CViewport::GetProjectionMatrix()),
+		op,
+		ImGuizmo::WORLD,
+		GUIZMO_MTX(mtx),
+		GUIZMO_MTX(delta));
+}
+
 bool Im3D::GizmoTrans(ImVec3V& translation)
 {
 	rage::Mat44V mtx(DirectX::XMMatrixTranslationFromVector(translation));
@@ -109,6 +146,56 @@ bool Im3D::GizmoTrans(ImVec3& translation)
 	return moved;
 }
 
+bool Im3D::GizmoBehaviour(bool& isDragging, const ImVec3V& planePos, const ImVec3V& planeNormal, ImVec3V& moveDelta, ImVec3V& startPos, ImVec3V& dragPos)
+{
+	static bool s_IsDragging = false;
+	static ImVec2 s_DragStartPos;
+
+	isDragging = false;
+
+	int mouse = ImGuiMouseButton_Left;
+	bool mouseDown = ImGui::IsMouseDown(mouse);
+
+	if (!s_IsDragging && !mouseDown)
+		return false;
+
+	ImVec2 mousePos = ImGui::GetMousePos();
+
+	if (ImGui::IsMouseClicked(mouse, false))
+	{
+		s_IsDragging = true;
+		s_DragStartPos = mousePos;
+	}
+
+	// Get world mouse rays for where mouse position was at start of dragging & now
+	rage::Vec3V mouseRayPos, mouseRayDir;
+	CViewport::GetWorldRayFromScreen(mouseRayPos, mouseRayDir, mousePos.x, mousePos.y);
+	rage::Vec3V startMouseRayPos, startMouseRayDir;
+	CViewport::GetWorldRayFromScreen(startMouseRayPos, startMouseRayDir, s_DragStartPos.x, s_DragStartPos.y);
+
+	// Cast rays on to dragging plane
+	rage::Vec3V mouseOnPlane;
+	rage::Vec3V startMouseOnPlane;
+	rageam::graphics::ShapeTest::RayIntersectsPlane(mouseRayPos, mouseRayDir, planePos, planeNormal, mouseOnPlane);
+	rageam::graphics::ShapeTest::RayIntersectsPlane(startMouseRayPos, startMouseRayDir, planePos, planeNormal, startMouseOnPlane);
+
+	// Im3D::Text(mouseOnPlane, "#1");
+	// Im3D::Text(startMouseOnPlane, "#2");
+
+	dragPos = mouseOnPlane;
+	startPos = startMouseOnPlane;
+	moveDelta = mouseOnPlane - startMouseOnPlane;
+	isDragging = s_IsDragging;
+
+	if (!mouseDown) // Finished dragging
+	{
+		s_IsDragging = false;
+		isDragging = false;
+		return true;
+	}
+	return false; // Still dragging
+}
+
 bool Im3D::Begin(const ImVec3V& startPos, bool background)
 {
 	ImGuiWindowFlags flags =
@@ -121,8 +208,8 @@ bool Im3D::Begin(const ImVec3V& startPos, bool background)
 		ImGuiWindowFlags_NoBringToFrontOnFocus |
 		ImGuiWindowFlags_AlwaysAutoResize;
 
-	bool isCulled;
-	ImVec2 sPos = WorldToScreen(startPos, isCulled);
+	ImVec2 sPos;
+	bool isCulled = !WorldToScreen(startPos, sPos);
 
 	ImGuiIO& io = ImGui::GetIO();
 	ImGui::SetNextWindowSize(io.DisplaySize);
--- projects/app/src/am/ui/im3d.h
+++ projects/app/src/am/ui/im3d.h
@@ -1,6 +1,14 @@
+//
+// File: im3d.h
+//
+// Copyright (C) 2023 ranstar74. All rights violated.
+//
+// Part of "Rage Am" Research Project.
+//
 #pragma once
 
 #include "imgui.h"
+#include "ImGuizmo.h"
 #include "rage/math/vecv.h"
 #include "rage/math/vec.h"
 
@@ -10,16 +18,25 @@ using ImMat44V = rage::Mat44V;
 
 namespace Im3D
 {
-	ImVec2 WorldToScreen(const ImVec3V& pos, bool& isCulled);
+	// Returns whether point is not culled (visible on screen and not behind camera)
+	bool WorldToScreen(const ImVec3V& worldPos, ImVec2& screenPos);
 
+	// Next drawn text will be centered at given world pos
+	void CenterNext();
 	void Text(const ImVec3V& pos, ConstString fmt, ...);
 	void TextBg(const ImVec3V& pos, ConstString fmt, ...);
 	void TextBgV(const ImVec3V& pos, ConstString fmt, va_list args);
 	void TextBgColored(const ImVec3V& pos, u32 col, ConstString fmt, ...);
 
 	bool GizmoTrans(ImMat44V& mtx);
+	bool Gizmo(const ImMat44V& mtx, ImMat44V& delta, ImGuizmo::OPERATION op = ImGuizmo::TRANSLATE);
 	bool GizmoTrans(ImVec3V& translation);
 	bool GizmoTrans(ImVec3& translation);
+
+	bool GizmoBehaviour(
+		bool& isDragging, 
+		const ImVec3V& planePos, const ImVec3V& planeNormal, 
+		ImVec3V& moveDelta, ImVec3V& startPos, ImVec3V& dragPos);
 
 	// Creates invisible window at given world coordinate, use ImGui functions to draw the rest
 	bool Begin(const ImVec3V& startPos, bool background = false);
