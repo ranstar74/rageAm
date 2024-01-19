9014fec7fc94222feea55a4926a5956fbfaee589
ranstar74 <falconr1125@gmail.com>
2024-01-20 02:51:37 +0300

    Add merge function for spdSphere

--- projects/app/src/rage/spd/sphere.h
+++ projects/app/src/rage/spd/sphere.h
@@ -1,12 +1,13 @@
 //
 // File: sphere.h
 //
-// Copyright (C) 2023 ranstar74. All rights violated.
+// Copyright (C) 2023-2024 ranstar74. All rights violated.
 //
 // Part of "Rage Am" Research Project.
 //
 #pragma once
 
+#include "rage/math/math.h"
 #include "rage/math/vecv.h"
 
 namespace rage
@@ -25,10 +26,25 @@ namespace rage
 			SetRadius(radius);
 		}
 
-		void SetCenter(const Vec3V& v) { CenterAndRadius.SetXYZ(v); }
-		Vec3V GetCenter() const { return Vec3V(CenterAndRadius); }
-
-		void SetRadius(const ScalarV& s) { CenterAndRadius.SetW(s.Get()); }
+		void    SetCenter(const Vec3V& v) { CenterAndRadius.SetXYZ(v); }
+		Vec3V   GetCenter() const { return Vec3V(CenterAndRadius); }
+		void    SetRadius(const ScalarV& s) { CenterAndRadius.SetW(s.Get()); }
 		ScalarV GetRadius() const { return CenterAndRadius.WWWW(); }
+
+		ScalarV DistanceTo(const spdSphere& other) const { return GetCenter().DistanceTo(other.GetCenter()); }
+
+		spdSphere Merge(const spdSphere& other) const
+		{
+			Vec3V to = other.GetCenter() - GetCenter();
+			float dist = to.Length().Get();
+			float lRadius = CenterAndRadius.W();
+			float rRadius = other.CenterAndRadius.W();
+			float min = Min(-lRadius, dist - rRadius);
+			float max = Max(lRadius, dist + rRadius);
+			float centre = (min + max) / 2.0f;
+			float newRadius = (max - min) / 2.0f;
+			Vec3V offsetToNewCenter = to * centre / Max(0.001f, dist);
+			return spdSphere(GetCenter() + offsetToNewCenter, newRadius);
+		}
 	};
 }
