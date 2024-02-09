26541f9c0cddd8ee56df1dfbab9cd92d7c9ff82d
ranstar74 <falconr1125@gmail.com>
2024-02-09 03:48:07 +0300

    Add protect page overrun protection in os allocator

--- projects/app/src/rage/system/osallocator.h
+++ projects/app/src/rage/system/osallocator.h
@@ -1,8 +1,7 @@
 #pragma once
 
 #include "allocator.h"
-
-// #define ENABLE_SYS_MEM_NODES
+#include "helpers/align.h"
 
 namespace rage
 {
@@ -13,66 +12,42 @@ namespace rage
 	 */
 	class sysMemOsAllocator : public sysMemAllocator
 	{
-#ifdef ENABLE_SYS_MEM_NODES
-		// We hold linked list to detect if block pointer is valid (needed for virtual / physical allocator)
+		DWORD m_PageSize = 0;
 
-		struct Node
+		void InitPageSize()
 		{
-			static constexpr u32 GUARD = MAKEFOURCC('R', 'A', 'G', 'E');
+			if (m_PageSize)
+				return;
 
-			u32 Guard;
-			Node* Prev;
-			Node* Next;
-			pVoid Block;
-
-			void DeleteSelfAndChildren()
-			{
-				if (Next)
-					Next->DeleteSelfAndChildren();
-				free(this);
-			}
-		};
-		Node* m_RootNode = nullptr;
-
-		Node* FindNode(pVoid block) const
-		{
-			Node* node = m_RootNode;
-			while (node)
-			{
-				AM_ASSERT(node->Guard == Node::GUARD, "sysMemOsAllocator::FindNode() -> Guard was trashed.");
-				if (node->Block == block)
-					return node;
-				node = node->Next;
-			}
-			return nullptr;
+			SYSTEM_INFO sysInfo;
+			GetSystemInfo(&sysInfo);
+			m_PageSize = sysInfo.dwPageSize;
 		}
-#endif
 
 	public:
-		~sysMemOsAllocator() override
-		{
-#ifdef ENABLE_SYS_MEM_NODES
-			// If any left then we've got memory leaks
-			m_RootNode->DeleteSelfAndChildren();
-#endif
-		}
-
 		pVoid Allocate(u64 size, u64 align, u32 type) override
 		{
-			pVoid block = _aligned_malloc(size, align);
+			InitPageSize();
 
-#ifdef ENABLE_SYS_MEM_NODES
-			// Insert block in linked list
-			Node* blockNode = (Node*)_aligned_malloc(sizeof Node, 16);
-			memset(blockNode, 0, sizeof Node);
-			blockNode->Block = block;
-			blockNode->Next = m_RootNode;
-			blockNode->Guard = Node::GUARD;
-			if (m_RootNode) m_RootNode->Prev = blockNode;
-			m_RootNode = blockNode;
-#endif
+			// We allocate two pages and put allocated block at the end of first page,
+			// padding is distance between beginning of page and allocated block.
+			size_t allocSize = ALIGN(size, m_PageSize);
+			size_t leftPadding = allocSize - size;
 
-			return block;
+			allocSize += m_PageSize; // Reserve protected page
+
+			char* block = static_cast<char*>(VirtualAlloc(
+				NULL,
+				allocSize,
+				MEM_COMMIT | MEM_RESERVE,
+				PAGE_READWRITE));
+
+			// Next adjacent block that we allocate is protected from reading / writing,
+			// which will help us to detect any buffer overrun
+			DWORD dwOldProtect;
+			VirtualProtect(block + allocSize - m_PageSize, m_PageSize, PAGE_NOACCESS, &dwOldProtect);
+
+			return block + leftPadding;
 		}
 
 		pVoid TryAllocate(u64 size, u64 align, u32 type) override
@@ -82,26 +57,15 @@ namespace rage
 
 		void Free(pVoid block) override
 		{
-#ifdef ENABLE_SYS_MEM_NODES
-			// Remove block from linked list
-			Node* blockNode = FindNode(block);
+			MEMORY_BASIC_INFORMATION mbi;
+			DWORD dwOldProtect;
 
-			if (blockNode->Next) blockNode->Next->Prev = blockNode->Prev;
-			if (blockNode->Prev) blockNode->Prev->Next = blockNode->Next;
-			if (m_RootNode == blockNode)
-				m_RootNode = blockNode->Next;
-
-			_aligned_free(blockNode);
-#endif
-			_aligned_free(block);
+			VirtualQuery(block, &mbi, sizeof mbi);
+			// Leave pages in reserved state, but free the physical memory
+			VirtualFree(mbi.AllocationBase, 0, MEM_DECOMMIT);
+			// Protect the address space, so no one can access those pages
+			VirtualProtect(mbi.AllocationBase, mbi.RegionSize, PAGE_NOACCESS, &dwOldProtect);
 		}
-
-#ifdef ENABLE_SYS_MEM_NODES
-		bool IsValidPointer(pVoid block) override
-		{
-			return FindNode(block) != nullptr;
-		}
-#endif
 
 		u64 GetMemoryUsed(u8 memoryBucket) override { return 0; }
 		u64 GetMemoryAvailable() override { return 0; }
