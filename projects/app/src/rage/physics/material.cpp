4ea7296569a68476b5ab68604ab4a34fce541951
ranstar74 <falconr1125@gmail.com>
2024-06-07 19:26:53 +0300

    Fix phMaterialMgr instance pattern

--- projects/app/src/rage/physics/material.cpp
+++ projects/app/src/rage/physics/material.cpp
@@ -5,10 +5,17 @@
 rage::phMaterialMgr* rage::phMaterialMgr::GetInstance()
 {
 #ifndef AM_STANDALONE
-			static phMaterialMgr* inst = gmAddress::Scan("48 8B 15 ?? ?? ?? ?? 4C 23 42 10")
-				.GetRef(3)
-				.To<decltype(inst)>();
-			return inst;
+	static phMaterialMgr* inst = 
+#if APP_BUILD_2699_16_RELEASE_NO_OPT
+		*gmAddress::Scan("48 8B 05 ?? ?? ?? ?? 48 8B 80 00 02 00 00 48 39 44 24 40", "rage::phMaterialMgr::sm_Instance")
+		.GetRef(3)
+		.To<phMaterialMgr**>();
+#else
+		gmAddress::Scan("48 8B 15 ?? ?? ?? ?? 4C 23 42 10", "rage::phMaterialMgr::sm_Instance")
+		.GetRef(3)
+		.To<rage::phMaterialMgr*>();
+#endif
+	return inst;
 #else
 	AM_UNREACHABLE("phMaterialMgr::GetInstance() -> Unsupported in standalone.");
 #endif
