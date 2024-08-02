79eead9af05685fbced3ae165e9db46606d6937a
ranstar74 <falconr1125@gmail.com>
2024-08-02 21:26:15 +0300

    Update FBXSDK vendor file

--- projects/app/vendor/fbxsdk.lua
+++ projects/app/vendor/fbxsdk.lua
@@ -1,15 +1,17 @@
-files { "fbxsdk/**.h", "fbxsdk/**.cpp" }
-includedirs { "fbxsdk/include/" }
+local sdkPath  = os.getenv("FBXSDK")
+
+files { sdkPath .. "/**.h", sdkPath .. "/**.cpp" }
+includedirs { sdkPath .. "/include/" }
 
 filter { "configurations:Debug" }
-	libdirs { "fbxsdk/lib/debug/" }
+	libdirs { sdkPath .. "/lib/x64/debug/" }
+	files { sdkPath .. "/lib/x64/debug/libfbxsdk.dll" }
 filter { "configurations:Release" }
-	libdirs { "fbxsdk/lib/release/" }
+	libdirs { sdkPath .. "/lib/x64/release/" }
+	files { sdkPath .. "/lib/x64/release/libfbxsdk.dll" }
 filter {}
 
 links { "alembic-md" }
 links { "libfbxsdk-md" }
 links { "libxml2-md" }
 links { "zlib-md" }
-
-files { "fbxsdk/lib/libfbxsdk.dll" }
